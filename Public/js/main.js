290f179360a64bee411e07897b59053a95a6ade4
amangupt01 <57101022+amangupt01@users.noreply.github.com>
2020-04-19 02:58:34 +0530

    adding some public files

---
+++ Public/js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+  $('.delete-article').on('click',function(e){
+    $target = $(e.target);
+    const id = $target.attr('data-id');
+    $.ajax({
+      type :'Delete',
+      url :'/articles/'+id,
+      success : function(response){
+        alert('Deleting Article');
+        window.location.href ='/';
+      },
+      error: function(err){
+        console.log(err);
+      }
+    });
+   
+  });
+
+});
